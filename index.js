c924ffc2f7434d403dc68d4ccac9da8c353a3328
rakeshcooper <rakeshmurali097@gmail.com>
2025-04-04 23:40:21 +0530

    update

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
-class Crud{
+class Crud{fgfg
     dataArray = []
     count = 0
+    val
     
     constructor(){
        this.todoInput = document.querySelector("#todoInput") 
        this.addBtn = document.querySelector(".addBtn")
        this.todoContainer = document.querySelector(".todoContainer")
-       this.elementTodo = `<li class="todo"><span class="todoValue">Test<span><span><button class="editBtn">Edit</button></span><span><input type="text"><button class="updateBtn">update</button></span><span><button class="deleteBtn">delete</button></span><span><button class="doneBtn">done</button></span></li>`;
     }
 
     createCrud(){
       this.addBtn.addEventListener("click",() => {
-        this.dataArray.push(todoInput.value)
-        // this.todoContainer.innerHTML = ""
-        this.todoContainer.innerHTML += this.elementTodo
+        this.dataArray.push(this.todoInput.value)
+        this.todoContainer.innerHTML += `<li class="todo"><span class="todoValue"><span>${this.todoInput.value}<span><button class="editBtn">Edit</button></span><span><input type="text"><button class="updateBtn">update</button></span><span><button class="deleteBtn">delete</button></span><span><button class="doneBtn">done</button></span></li>`
         this.todo = document.querySelector(".todo")
-        this.todo.firstChild.style.backgroundColor = "red"
-        this.todo.firstChild.textContent = todoInput.value;
-        console.log(this.elementTodo);
         console.log(this.dataArray);
         
             
