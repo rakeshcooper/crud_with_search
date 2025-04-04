e7b994b650744a5ab66d102d92122c50041450ce
rakeshcooper <rakeshmurali097@gmail.com>
2025-04-05 00:19:59 +0530

    update

--- index.js
+++ index.js
@@ -12,17 +12,27 @@ class Crud{fgfg
     createCrud(){
       this.addBtn.addEventListener("click",() => {
         this.dataArray.push(this.todoInput.value)
-        this.todoContainer.innerHTML += `<li class="todo"><span class="todoValue"><span>${this.todoInput.value}<span><button class="editBtn">Edit</button></span><span><input type="text"><button class="updateBtn">update</button></span><span><button class="deleteBtn">delete</button></span><span><button class="doneBtn">done</button></span></li>`
+        this.todoContainer.innerHTML += `<li class="todo"><span class="todoValue"><span>${this.todoInput.value}<span><button class="editBtn">Edit</button></span><span class="updatebox"><input type="text"><button class="updateBtn">update</button></span><span><button class="deleteBtn">delete</button></span><span><button class="doneBtn">done</button></span></li>`
         this.todo = document.querySelector(".todo")
         console.log(this.dataArray);
-        
-            
+        this.updateCrud()
       })
-      
-      
     }
 
     updateCrud(){
+       this.todoContainer.addEventListener("click",(e) => {
+            if(e.target.className == "editBtn"){
+                e.target.style.display = "none"
+                e.target.parentElement.nextSibling.style.display = "inline-block"
+
+            } else if(e.target.className == "updateBtn"){
+                e.target.parentElement.style.display = "none"
+                console.log(e.target.previousSibling.value);
+                
+                e.target.parentElement.previousSibling.firstChild.style.display = "inline-block"
+            }
+            
+       }) 
         
     }
 
