c6a090584e0142d8d0b4c0c973b4b917b4c11c7d
rakeshcooper <rakeshmurali097@gmail.com>
2025-04-05 00:41:57 +0530

    new Update

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ class Crud{fgfg
     createCrud(){
       this.addBtn.addEventListener("click",() => {
         this.dataArray.push(this.todoInput.value)
-        this.todoContainer.innerHTML += `<li class="todo"><span class="todoValue"><span>${this.todoInput.value}<span><button class="editBtn">Edit</button></span><span class="updatebox"><input type="text"><button class="updateBtn">update</button></span><span><button class="deleteBtn">delete</button></span><span><button class="doneBtn">done</button></span></li>`
+        this.todoContainer.innerHTML += `<li class="todo"><span class="todoValue">${this.todoInput.value}</span><span><button class="editBtn">Edit</button></span><span class="updatebox"><input type="text"><button class="updateBtn">update</button></span><span><button class="deleteBtn">delete</button></span><span><button class="doneBtn">done</button></span></li>`
         this.todo = document.querySelector(".todo")
         console.log(this.dataArray);
         this.updateCrud()
@@ -27,9 +27,11 @@ class Crud{fgfg
 
             } else if(e.target.className == "updateBtn"){
                 e.target.parentElement.style.display = "none"
-                console.log(e.target.previousSibling.value);
-                
+                this.index = this.dataArray.indexOf(e.target.parentElement.previousSibling.previousSibling.innerText)
+                this.dataArray[this.index] = e.target.previousSibling.value
+                e.target.parentElement.previousSibling.previousSibling.innerText = e.target.previousSibling.value
                 e.target.parentElement.previousSibling.firstChild.style.display = "inline-block"
+               // console.log(this.dataArray);    
             }
             
        }) 
